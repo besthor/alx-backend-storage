b6e15f85cff95d61b107eaf8f3e904657a14cfd2
Besthor <111004790+besthor@users.noreply.github.com>
2023-07-16 19:39:13 +0100

    Updated  3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
--- This SQL script select band_name, and lifespan column which is difference
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- SQL script that lists all bands with Glam rock as their main style
+-- ranked by their longevity
+SELECT band_name,
+    COALESCE(split, 2020) - formed as lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
