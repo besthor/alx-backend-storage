8ab4ebe6e450667271d66b583f00e7d06ab7c57d
besthor <igbebestor7@gmail.com>
2023-07-19 00:00:51 +0000

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+
+    top_st = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_st
