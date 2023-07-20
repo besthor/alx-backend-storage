00518b622fe17eb6faeb708cb77e5f57a15d4bec
besthor <igbebestor7@gmail.com>
2023-07-20 04:25:51 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
-""" Module for Implementing an expiring web cache and tracker """
+""" Advanced - Module for Implementing an expiring
+    web cache and tracker
+"""
 
-from functools import wraps
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
-client = redis.Redis()
+rd = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Decortator to count how many request has been made"""
+    """ Counting with decorators how many times a request
+        has been made
+    """
 
     @wraps(method)
     def wrapper(url):
-        """ Function wrapper """
-        client.incr(f"count:{url}")
-        cached_html = client.get(f"cached:{url}")
+        """ Wrapper for decorator functionality """
+        rd.incr(f"count:{url}")
+        cached_html = rd.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
 
         html = method(url)
-        client.setex(f"cached:{url}", 10, html)
+        rd.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
@@ -29,7 +33,8 @@ def count_requests(method: Callable) -> Callable:
 
 @count_requests
 def get_page(url: str) -> str:
-    """Gets the html content of a web page
+    """ requests module to obtain the HTML
+        content of a particular URL and returns it.
     """
     req = requests.get(url)
     return req.text
